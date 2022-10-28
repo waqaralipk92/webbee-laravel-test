2d64e4f09e34cb11137bed8d6e429d5104090312
waqaralipk <waqar.ali@thevisionspark.com>
2022-10-28 19:42:56 +0500

    completed other tasks

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -1,12 +1,13 @@
-<?php
-
-
-namespace App\Models;
+<?php namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class MenuItem extends Model
 {
-
+    public function children() : HasMany
+    {
+        return $this->hasMany(MenuItem::class, 'parent_id');
+    }
 }
